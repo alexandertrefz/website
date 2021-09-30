4324272e7c309b362c6ee1bce8be4bb260d2769b
Alexander Trefz <alexander.trefz@gmail.com>
2021-09-30 12:23:21 +0200

    Add content to About page

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -7,4 +7,49 @@
 	<meta name="description" content="Everything you need to know about Alexander Trefz." />
 </svelte:head>
 
-<Headline>About Me</Headline>
+<article class="about">
+	<Headline>About Me</Headline>
+	<p>Hi! I am Alexander Trefz, a Software Engineer specialising in Interface Development.</p>
+	<p>
+		I have been working in software engineering since 2010, and since then have worked on Apps &
+		Websites used by millions of people.
+	</p>
+	<p>
+		For a few years I have been using my free time to work on my programming language project,
+		called
+		<a target="_blank" href="https://alexandertrefz.github.io/essence/">Essence</a>.
+	</p>
+	<p>
+		On this blog I write, ramble, and rant about Software Development, Interface Design, and some
+		other things.
+	</p>
+</article>
+
+<style lang="scss">
+	.about {
+		display: grid;
+		gap: 30px;
+
+		justify-self: center;
+		max-width: var(--content-width);
+
+		p {
+			font-family: var(--font-article-text);
+			font-size: calc(var(--base-font-size) * 1.125);
+			line-height: 1.3;
+			text-align: var(--alignment);
+		}
+
+		a {
+			color: var(--color-highlight-intense);
+			text-decoration-style: underline;
+			text-decoration-color: transparent;
+
+			transition: text-decoration-color 200ms linear;
+
+			&:hover {
+				text-decoration-color: var(--color-highlight-intense);
+			}
+		}
+	}
+</style>
