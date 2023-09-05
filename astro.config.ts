22167666ef8116ad96e728826b6bd6441f47f183
Alexander Trefz <alexander.trefz@gmail.com>
2023-09-05 17:25:06 +0200

    Upgrade to Astro 3

--- astro.config.ts
+++ astro.config.ts
@@ -8,9 +8,6 @@ import serviceWorker from "astrojs-service-worker"
 export default defineConfig({
 	site: "https://alexander.trefz.me",
 	trailingSlash: "always",
-	experimental: {
-		viewTransitions: true
-	},
 	build: {
 		format: "directory",
 	},
