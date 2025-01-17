5e32157e313e7d1e69d2302fcdcc54542523309b
Alexander Trefz <alexander.trefz@gmail.com>
2025-01-18 00:08:25 +0100

    Use Astro Collections

---
+++ .astro/content-modules.mjs
@@ -0,0 +1,4 @@
+
+export default new Map([
+["src/pages/blog/how-performance-sets-truly-great-apps-apart.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fpages%2Fblog%2Fhow-performance-sets-truly-great-apps-apart.mdx&astroContentModuleFlag=true")]]);
+		
