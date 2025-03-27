e09c9bc09a254e4a2cb8cfb527368ab8146269fa
Alexander Trefz <alexander.trefz@gmail.com>
2025-03-27 10:05:30 +0100

    Fix .astro handling

--- .astro/content-modules.mjs
+++
@@ -1,4 +0,0 @@
-
-export default new Map([
-["src/pages/blog/how-performance-sets-truly-great-apps-apart.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fpages%2Fblog%2Fhow-performance-sets-truly-great-apps-apart.mdx&astroContentModuleFlag=true")]]);
-		
